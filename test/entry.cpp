ebc6928e640f7dbfa3a8ba4334e7f9ec99633e9a
Vasie <59412971+Vasie1337@users.noreply.github.com>
2025-03-24 19:38:38 +0100

    Add core logic for test suite

--- test/entry.cpp
+++ test/entry.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include "../core/cs2/parser.h"
 
 int main()
 {
+	std::vector<cs2::Triangle> triangles;
 	
+	std::ifstream in("de_dust2.tri", std::ios::binary);
+	if (!in)
+	{
+		std::cerr << "Failed to open file: dust.tri" << std::endl;
+		return 1;
+	}
+
+	cs2::Triangle triangle = {};
+	while (in.read(reinterpret_cast<char*>(&triangle), sizeof(cs2::Triangle)))
+	{
+		triangles.push_back(triangle);
+	}
+
+	std::cout << "Loaded " << triangles.size() << " triangles." << std::endl;
+
 
 	return 0;
 }
